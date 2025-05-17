3985904c287082b6710cec77b604b1f950cd0d3e
Paulo Ricardo <paulorihcardo18@gmail.com>
2025-05-17 00:16:55 -0300

    criaçao do modal de deletar user

---
+++ src/components/DeleteUserDialog.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="text-center pa-4">
+    <v-dialog v-model="dialog" width="auto">
+      <v-card>
+        <v-card-title class="text-h5 ml-2"> Confirmar exclusão </v-card-title>
+
+        <v-card-text>
+          Deseja realmente excluir o usuário <strong>{{ user.name }}?</strong>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text @click="dialog = false">Cancelar</v-btn>
+          <v-btn color="error" @click="confirmDelete">Excluir</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: Boolean,
+    user: Object,
+  },
+  emits: ["update:modelValue", "confirm-delete"],
+  computed: {
+    dialog: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit("update:modelValue", value);
+      },
+    },
+  },
+  methods: {
+    confirmDelete() {
+      this.$emit("confirm-delete", this.user.id);
+      this.dialog = false;
+    },
+  },
+};
+</script>
